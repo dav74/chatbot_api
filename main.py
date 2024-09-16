43be942dd47d54604c5cb3491674a2936cc6827c
dav74 <dav74130@gmail.com>
2024-09-16 21:19:47 +0200

    update cors

--- main.py
+++ main.py
@@ -14,7 +14,9 @@ class Question(BaseModel):
 app = FastAPI()
 origins = [
     "http://127.0.0.1:5500",
-    "http://localhost:5500"
+    "http://localhost:5500",
+    "https://pixees.fr/informatiquelycee/chatbot/",
+    "https://pixees.fr/"
 ]
 
 app.add_middleware(
