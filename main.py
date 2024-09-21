10d54fff02de072e8c5a94e4df85a715ffd3cf93
dav74 <dav74130@gmail.com>
2024-09-21 19:21:42 +0200

    update

--- main.py
+++ main.py
@@ -2,28 +2,29 @@ from dotenv import load_dotenv
 import os
 from llama_index.core import VectorStoreIndex, StorageContext, SimpleDirectoryReader, load_index_from_storage
 from llama_index.embeddings.huggingface_api import HuggingFaceInferenceAPIEmbedding
-
+from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 from llama_index.llms.groq import Groq
 from llama_index.core import Settings
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
+from llama_index.core.llms import ChatMessage
 
-class Question(BaseModel):
-    question: str
+
+class History(BaseModel):
+    role: str
+    content: str
+
+class Request(BaseModel):
+    chat_message: list[History]
+    query : str
 
 app = FastAPI()
-origins = [
-    "http://127.0.0.1:5500",
-    "http://localhost:5500",
-    "https://pixees.fr/informatiquelycee/chatbot/",
-    "https://pixees.fr/"
-]
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=["*"],
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
@@ -31,7 +32,7 @@ app.add_middleware(
 load_dotenv()
 Settings.llm = Groq(temperature=0.8,model="llama-3.1-70b-versatile", api_key=os.getenv('GROQ_API_KEY'))
 Settings.embed_model = HuggingFaceInferenceAPIEmbedding(model_name="OrdalieTech/Solon-embeddings-large-0.1", token = os.getenv('HF_API_KEY'))
-
+#Settings.embed_model = HuggingFaceEmbedding(model_name="OrdalieTech/Solon-embeddings-large-0.1")
 try:
     storage_context = StorageContext.from_defaults(persist_dir = './storage')
     res_index = load_index_from_storage(storage_context)
@@ -46,9 +47,7 @@ if not index_loaded:
     res_index.storage_context.persist(persist_dir = './storage')
     print('index creation completed')
 
-chat_engine = res_index.as_chat_engine(
-    chat_mode="context",
-    system_prompt = (
+prompt =  (
         "Tu es un assistant spécialisé dans l'enseignement de la spécialité Numérique et sciences informatiques en classe de première et de terminal"
         'Tu as un bon niveau en langage Python'
         'Ton interlocuteur est un élève qui suit la spécialité nsi en première et en terminale'
@@ -61,9 +60,19 @@ chat_engine = res_index.as_chat_engine(
         'Tu dois uniquement répondre en langue française'
         'Tu dois utiliser le format markdown pour tes réponses'
     )
-)
 
-@app.post('/ask')
-def ask(question : Question):
-    response = chat_engine.chat(question.question)
+def get_chat_history(chat_messages):  
+    chat_history = []  
+    for message in chat_messages:  
+        chat_history.append(ChatMessage(content=message.content, role=message.role))  
+    return chat_history
+
+@app.post('/request')
+def request(req: Request):
+    chat_engine = res_index.as_chat_engine(
+        chat_mode="context",
+        system_prompt = prompt,
+    )
+    chat_history = get_chat_history(req.chat_message)
+    response = chat_engine.chat(req.query, chat_history=chat_history)
     return {"response":response.response}
